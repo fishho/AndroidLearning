f6bc2dc23c1bd5c1fb6ba92ff465fe16ae470bcc
Dfish <2012chenjian@gmail.com>
2016-05-20 15:15:01 +0800

    add a notepad demo,details need to be polished

---
+++ notepad/src/main/java/com/cfish/notepad/DBHelper.java
@@ -0,0 +1,7 @@
+package com.cfish.notepad;
+
+/**
+ * Created by GKX100217 on 2016/5/19.
+ */
+public class DBHelper {
+}
---
+++ notepad/src/main/java/com/cfish/notepad/DeleteAsyncTask.java
@@ -0,0 +1,7 @@
+package com.cfish.notepad;
+
+/**
+ * Created by GKX100217 on 2016/5/20.
+ */
+public class DeleteAsyncTask {
+}
